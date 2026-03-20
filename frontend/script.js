e1e19d02a87c00f303734386d49c342b5abca898
Manideep0004 <forgetyt97@gmail.com>
2026-03-20 07:16:26 +0530

    Click button → show recommendations

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,28 @@
 async function loadRec() {
 
+    loadRow("rec1", 10)
+    loadRow("rec2", 20)
+    loadRow("rec3", 30)
+
+}
+
+async function loadRow(divId, index) {
+
     const res = await fetch(
-        "http://127.0.0.1:8000/recommend/10"
+        `http://127.0.0.1:8000/recommend/${index}`
     )
 
     const data = await res.json()
 
-    const div = document.getElementById("products")
+    const div =
+        document.getElementById(divId)
 
     div.innerHTML = ""
 
     data.forEach(p => {
 
-        const card = document.createElement("div")
+        const card =
+            document.createElement("div")
 
         card.className = "card"
 
